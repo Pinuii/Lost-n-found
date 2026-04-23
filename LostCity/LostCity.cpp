39118914b904843f83065bbdf979d44df005be78
Pette <pfarrapilha@gaming.tech>
2026-04-23 13:06:19 +0200

    added gameview

--- LostCity/LostCity.cpp
+++ LostCity/LostCity.cpp
@@ -2,20 +2,40 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML works!");
+	//sf::CircleShape shape(100.f);
+	//shape.setFillColor(sf::Color::Green);
+	sf::Texture background;
+	background.loadFromFile("assets/background.png");
+	sf::View view(sf::FloatRect({ 0.f, 0.f }, { 800.f, 600.f }));
+	float speed = 600.f;
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
+	sf::Clock clock;
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	while (window.isOpen())
+	{
+		float dt = clock.restart().asSeconds();
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+				window.close();
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Z))
+			view.move({ 0.f, -speed * dt });
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+			view.move({ 0.f, speed * dt });
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q))
+			view.move({ -speed * dt, 0.f });
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+			view.move({ speed * dt, 0.f });
+
+		// Appliquer la vue
+		window.setView(view);
+
+		window.clear();
+		sf::Sprite backgroundSprite(background);
+		window.draw(backgroundSprite);
+		//window.draw(shape);
+		window.display();
+	}
 }
